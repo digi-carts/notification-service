32c861b3dcddc6fb20f323211cc5a23fd3be9bb6
Ananthu PM <ananthupmadhu@gmail.com>
2026-08-20 00:49:31 +0530

    fix: run Cucumber component tests against in-memory H2
    CI mvn test now boots Spring with a test profile so JPA services do not need Postgres.

--- src/test/java/com/digicart/notification/cucumber/CucumberSpringConfiguration.java
+++ src/test/java/com/digicart/notification/cucumber/CucumberSpringConfiguration.java
@@ -2,20 +2,12 @@ package com.digicart.notification.cucumber;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import com.digicart.notification.exception.GlobalExceptionHandler;
-import com.digicart.notification.controller.HealthController;
-import com.digicart.notification.controller.NotificationConfigController;
-import com.digicart.notification.service.NotificationConfigService;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
-@WebMvcTest(controllers = { HealthController.class, NotificationConfigController.class })
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc(addFilters = false)
-@Import(GlobalExceptionHandler.class)
+@ActiveProfiles("test")
 public class CucumberSpringConfiguration {
-    @MockBean
-    NotificationConfigService notificationConfigService;
-
 }
